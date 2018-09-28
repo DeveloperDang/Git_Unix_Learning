0ab2d607598213fc102a1353a94e03dc0957d424
774090882@qq.com <774090882@qq.com>
2018-09-28 13:45:21 +0800

    线程同步互斥锁，信号量，条件变量

--- thread.c
+++ thread.c
@@ -153,14 +153,14 @@ void increse_num(void) {
 
 #endif
 
-#if 1   
+#if 0
 
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <semaphore.h>
 
-#define MAXSIZE 10
+#define MAXSIZE 1000000
 
 int stack[MAXSIZE];
 int size = 0;
@@ -200,6 +200,141 @@ int main(void) {
 
     return 0;
 }
+
+
+#endif
+
+#if 0
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define SIZE 10
+
+sem_t sem;
+
+void funProducer()
+{
+	int i;
+	//for (i = 0; i < SIZE; i++)
+	for(;;)
+	{
+		sem_post(&sem);	        //此时回给信号量加1操作，如果信号量为0，则该线程循环后，信号量一直增加到SIZE-1
+	}	
+}
+
+void funConsumer()
+{
+	int i = 0; 
+	while(i < SIZE)         //while循环得到目的是当sem为0且生产者不生成时，不在进行sem_wait，不然线程会阻塞，导致整个进程不能退出
+	{
+		sem_wait(&sem);       // 如果sem是0的话，此时sem_wait属于阻塞状态，如果sem是大于0的话，则执行下面就语句
+		printf("%d * %d = %d\n", i, i, i * i);
+		i++;	
+		sleep(1);
+	}
+	while(1)               //这种写法会导致sem为0，且生产者不生产之后，导致整个线程阻塞，进程无法退出
+	{
+		sem_wait(&sem);       // 如果sem是0的话，此时sem_wait属于阻塞状态，如果sem是大于0的话，则执行下面就语句
+		printf("2%d * %d = %d\n", i, i, i * i);
+		i++;	
+		sleep(1);
+		
+	}
+}
+
+int main(void)
+{
+	pthread_t producer, consumer;    //线程id
+	sem_init(&sem, 0, 0);  //出事换信号量
+	pthread_create(&producer, NULL, (void *)&funProducer, NULL);   //创建线程1	
+	pthread_create(&consumer, NULL, (void *)&funConsumer, NULL);   //创建线程2
+	pthread_join(producer, NULL);                           //等待线程1结束，阻塞
+	pthread_join(consumer, NULL);                           //等待线程2结束，阻塞
+	sem_destroy(&sem);                                 //销毁信号量
+	return 0;
+}
+
+#endif
+
+
+#if 1
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>  
+#include <stdio.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <errno.h>
+
+int index = 1;
+
+pthread_mutex_t lock;
+pthread_cond_t cond;
+
+void* fun1(void* p)
+{
+    while (index < 50)
+    {
+        pthread_mutex_lock(&lock);    
+        index++;
+        printf("In fun1 : %d\n",index);
+        pthread_cond_signal(&cond);//当有变化后，使用signal通知wait函数
+        pthread_mutex_unlock(&lock);
+        usleep(0.1);
+    }
+}
+
+void* fun2(void* p)
+{
+    while (index < 50)
+    {       
+        pthread_mutex_lock(&lock);    
+        index++;
+        printf("In fun2 : %d\n",index);
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&lock);
+        usleep(0.1);
+    }
+}
+
+void* fun3(void*p)
+{
+    int i=0;
+    while (i < 70)
+    {
+         pthread_mutex_lock(&lock);
+         while ( index % 3 != 0)
+         {
+             pthread_cond_wait(&cond, &lock);//如果获得了互斥锁，但是条件不合适的话，wait会释放锁，不往下执行。当变化后，条件合适，将直接获得锁。
+         }
+
+         printf("%d\n",index/3);
+
+         i++;
+         pthread_mutex_unlock(&lock);
+         usleep(0.1);
+    }
+}
+
+int main(){
+
+    pthread_mutex_init(&lock, NULL);
+    pthread_cond_init(&cond, NULL);
+
+    pthread_t tid1,tid2,tid3;
+    pthread_create(&tid1, NULL, fun1, NULL);
+    pthread_create(&tid2, NULL, fun2, NULL);
+    pthread_create(&tid3, NULL, fun3, NULL);
+
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+    pthread_join(tid3, NULL);
+
+    return 0;
+}
+
 
 
 #endif
